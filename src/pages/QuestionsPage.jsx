0eafad4a572f6ae9cc7d3e59317c65a702b29405
Isaac Ayomi <Okunlolaa3@outlook.com>
2025-07-23 15:07:24 +0100

    feat: add OptionsButton and OptionsList components for improved question display and navigation

--- src/pages/QuestionsPage.jsx
+++ src/pages/QuestionsPage.jsx
@@ -1,20 +1,36 @@
 import { useDispatch, useSelector } from "react-redux";
+import Button from "../ui/Button";
 import LeftPane from "../ui/LeftPane";
 import { nextQuestion } from "../features/QuestionSlice";
+import OptionsButton from "../ui/optionsButton";
+import OptionsList from "../ui/OptionsList";
 
 function QuestionsPage() {
   const dispatch = useDispatch();
+  const { questions: questionObject } = useSelector(
+    (state) => state.question || {},
+  );
   const { questionIndex } = useSelector((state) => state.question);
+
+  const options = questionObject?.questions?.[questionIndex]?.options;
+  console.log(options);
+
   return (
-    <button
-      onClick={() => {
-        dispatch(nextQuestion());
-        console.log("Next question dispatched");
-        console.log("Current subject index:", questionIndex);
-      }}
-    >
-      Next Question
-    </button>
+    <div className="flex flex-col items-center justify-center">
+      <li className="list-none">
+        <OptionsList />
+      </li>
+
+      <OptionsButton
+        onClick={() => {
+          dispatch(nextQuestion());
+          console.log("Next question dispatched");
+          console.log("Current subject index:", questionIndex);
+        }}
+      >
+        Next Question
+      </OptionsButton>
+    </div>
   );
 }
 
