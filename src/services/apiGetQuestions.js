aed07ae8b8b4bdd4cb986433aa46372c440f3044
Isaac Ayomi <Okunlolaa3@outlook.com>
2025-07-30 21:51:00 +0100

    Deploy working quiz API to Render and update fetch function

--- src/services/apiGetQuestions.js
+++ src/services/apiGetQuestions.js
@@ -1,23 +1,25 @@
-// export async function getQuestion(subject, index) {
-//   const res = await fetch(`http://localhost:8000/quizzes?subject=${subject}`);
-//   if (!res.ok) throw new Error("Failed to fetch questions");
-//   const data = await res.json();
-
-//   return data[index];
-// }
-
-export async function getQuestion(subject) {
-  const res = await fetch("/quizzes.json");
-  if (!res.ok) throw new Error("Failed to fetch questions");
-
-  const data = await res.json();
-  console.log("Fetched data:", data);
-
-  const filtered = data.filter(
-    (q) => q.subject.toLowerCase() === subject.toLowerCase(),
+export async function getQuestion(subject, index) {
+  const res = await fetch(
+    `https://quiz-api-1-5jqh.onrender.com/quizzes?subject=${subject}`,
   );
+  if (!res.ok) throw new Error("Failed to fetch questions");
+  const data = await res.json();
 
-  console.log("Filtered questions:", filtered);
-
-  return filtered; // ✅ Return all questions for that subject
+  return data[index];
 }
+
+// export async function getQuestion(subject) {
+//   const res = await fetch("/quizzes.json");
+//   if (!res.ok) throw new Error("Failed to fetch questions");
+
+//   const data = await res.json();
+//   console.log("Fetched data:", data);
+
+//   const filtered = data.filter(
+//     (q) => q.subject.toLowerCase() === subject.toLowerCase(),
+//   );
+
+//   console.log("Filtered questions:", filtered);
+
+//   return filtered; // ✅ Return all questions for that subject
+// }
