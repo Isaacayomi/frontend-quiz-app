190c19f147b1957b952bb60bc4db5578ae4511ff
Isaac Ayomi <Okunlolaa3@outlook.com>
2025-07-30 20:28:54 +0100

    fix

--- src/services/apiGetQuestions.js
+++ src/services/apiGetQuestions.js
@@ -1,7 +1,18 @@
+// export async function getQuestion(subject, index) {
+//   const res = await fetch(`http://localhost:8000/quizzes?subject=${subject}`);
+//   if (!res.ok) throw new Error("Failed to fetch questions");
+//   const data = await res.json();
+
+//   return data[index];
+// }
+
 export async function getQuestion(subject, index) {
-  const res = await fetch(`http://localhost:8000/quizzes?subject=${subject}`);
+  const res = await fetch("/quizzes.json");
   if (!res.ok) throw new Error("Failed to fetch questions");
+
   const data = await res.json();
 
-  return data[index];
+  const filtered = data.filter((q) => q.subject === subject);
+
+  return filtered[index];
 }
